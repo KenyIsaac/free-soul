c06f5c0aaf0d29ab1d1f94d65df3d94888b67266
Keny Isaac <keny.isaac.lm@hotmail.com>
2022-08-07 10:49:10 -0500

    [fix] fixed colurs in navbarmobile

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -5,23 +5,23 @@ import { IoClose } from 'react-icons/io5';
 
 const NavMobile = ({ setNavMobile }) => {
   return (
-    <nav className='lg:hidden bg-[#251f3f] w-full h-full'>
+    <nav className='lg:hidden bg-[#e3b0f6] w-full h-full'>
       <IoClose
         onClick={() => setNavMobile(false)}
         className='text-3xl absolute left-4 top-6 cursor-pointer'
       />
-      <ul className='flex flex-col items-center justify-center space-y-8 h-full font-secondary'>
-        <li className='text-lg'>
-          <a href='#'>Home</a>
+      <ul className='flex flex-col items-center justify-center space-y-8 h-full font-secondary text-white'>
+        <li className='text-lg text-[white]'>
+          <a href='#'>Inicio</a>
         </li>
-        <li className='text-lg'>
-          <a href='#'>Company</a>
+        <li className='text-lg text-[white]'>
+          <a href='#'>Sobre nosotros</a>
         </li>
-        <li className='text-lg'>
-          <a href='#'>Features</a>
+        <li className='text-lg text-[white]'>
+          <a href='#'>Experiencia de usuarios</a>
         </li>
-        <li className='btn'>
-          <a href='#'>Sign up</a>
+        <li className='btn text-[white]'>
+          <a href='#'>Inicia Sesión</a>
         </li>
       </ul>
     </nav>
